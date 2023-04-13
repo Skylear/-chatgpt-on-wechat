fa2dca5fa75344ecc9f8d1de15e4cedd6b2d44ea
NewProject <814034570@qq.com>
2023-04-13 10:29:45 +0800

    Update chat_channel.py

--- channel/chat_channel.py
+++ channel/chat_channel.py
@@ -197,9 +197,9 @@ class ChatChannel(Channel):
                         return self._decorate_reply(context, reply)
                     if context.get("isgroup", False):
                         reply_text = '@' +  context['msg'].actual_user_nickname + ' ' + reply_text.strip()
-                        reply_text =   reply_text + '\n\n' + conf().get("group_chat_reply_prefix", "")
+                        reply_text =   reply_text + conf().get("group_chat_reply_prefix", "")
                     else:
-                        reply_text =  reply_text + '\n\n' + conf().get("single_chat_reply_prefix", "")
+                        reply_text =  reply_text  + conf().get("single_chat_reply_prefix", "")
                     reply.content = reply_text
                 elif reply.type == ReplyType.ERROR or reply.type == ReplyType.INFO:
                     reply.content = "["+str(reply.type)+"]\n" + reply.content
@@ -215,7 +215,7 @@ class ChatChannel(Channel):
     def _send_reply(self, context: Context, reply: Reply):
         if reply and reply.type:
             e_context = PluginManager().emit_event(EventContext(Event.ON_SEND_REPLY, {
-                'channel': self, 'context': reply, 'reply': context}))
+                'channel': self, 'context': context, 'reply': reply}))
             reply = e_context['reply']
             if not e_context.is_pass() and reply and reply.type:
                 logger.debug('[WX] ready to send reply: {}, context: {}'.format(reply, context))
